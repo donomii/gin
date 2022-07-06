8b810a679e80a28386da65aaf3bff345dd8ffac0
Jeremy Price <jeremy.price@grafana.com>
2022-07-06 04:02:36 +0200

    .

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
 Package gin implements a HTTP web framework called gin.
 
-See https://gin-gonic.com/ for more information about gin.
+See https://donomii.com/ for more information about gin.
 */
-package gin // import "github.com/gin-gonic/gin"
+package gin // import "github.com/donomii/gin"
